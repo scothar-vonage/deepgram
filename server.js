113a2d7997708779f5cd68965d112ff3874f8b52
Scott Harris <sharris@twilio.com>
2023-04-29 16:18:53 -0700

    changed to es6

--- server.js
+++ server.js
@@ -1,33 +1,63 @@
 "use strict";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const expressWs = require("express-ws")(app);
-const { Deepgram } = require("@deepgram/sdk");
+import { v4 as uuidv4 } from "uuid";
+import express from "express";
+import bp from "body-parser";
+const { json } = bp;
 
-const silenceThreshold = 0.02; // Adjust this value based on your needs
-const silenceDuration = 1000; // Duration of silence in milliseconds
-require("dotenv").config();
-const { decodeAudioData } = import("audio-decode");
-const { default: AudioBuffer } = import("audio-buffer");
+import pkg from "@deepgram/sdk";
+const { Deepgram } = pkg;
+
+import decodeAudio from "audio-decode";
+import AudioBuffer from "audio-buffer";
+
+import expressWsFactory from "express-ws";
+import dotenv from "dotenv";
+dotenv.config();
+
+const silenceThreshold = 0.09; // Adjust this based on testing
+
+const app = express();
+const expressWs = expressWsFactory(app);
 
 const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
 
 //Actively listen to the socket and get a transcript
 let listen = false;
+let listenStart = null;
+let listenDuration;
 let silenceStart = null;
-let startTime;
+let silenceDuration = 1000; // Duration of silence in milliseconds
+
+let db = [];
+
+//your ngrok domain name
 const url = "e165-98-37-112-87.ngrok-free.app";
 
-app.use(bodyParser.json());
-app.get("/input", (req, resp) => {});
+app.use(json());
+app.get("/input", (req, res) => {
+  listen = true;
+  listenStart = new Date().getTime();
+  listenDuration = req.query.timeout;
+  silenceDuration = req.query.max_silence;
+
+  const uniqueId = generateUniqueId().replace(/-/g, "");
+  const ret = {
+    message: "success",
+    listen_id: uniqueId,
+  };
+
+  console.log(
+    `Starting listening for ${uniqueId} for ${listenDuration} and silence of ${silenceDuration}`
+  );
+  res.status(200).json(ret);
+});
 
 app.get("/answer", (req, res) => {
   let nccoResponse = [
     {
       action: "talk",
-      text: "Please wait while we connect you to the echo server",
+      text: "How can I help you?",
     },
     {
       action: "connect",
@@ -64,10 +94,25 @@ app.ws("/socket", (ws, req) => {
 
     dgs.addListener("open", () => {
       console.log("deepgramSocket opened!");
+
       ws.on("message", (msg) => {
-        processMedia(msg);
-        if (dgs.getReadyState() == 1) {
-          dgs.send(msg);
+        const currentTime = new Date().getTime();
+
+        if (listen) {
+          if (currentTime - listenStart > listenDuration) {
+            console.log("Listen timeout expired.");
+            stopListening();
+          }
+
+          processMedia(msg);
+          if (dgs.getReadyState() == 1) {
+            dgs.send(msg);
+          } else {
+            console.error(
+              "Deepgram socket not ready. State: ",
+              dgs.getReadyState()
+            );
+          }
         }
       });
     });
@@ -93,19 +138,18 @@ app.ws("/socket", (ws, req) => {
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-async function processMedia(msg) {
+function processMedia(msg) {
   const audioBuffer = new AudioBuffer({
     numberOfChannels: 1,
     length: msg.length / 2,
     sampleRate: 16000,
   });
 
-  // Decode Linear16 (PCM) audio data to raw audio samples
-  const int16Array = new Int16Array(message.buffer);
+  const int16Array = new Int16Array(msg.buffer);
   for (let i = 0; i < int16Array.length; i++) {
     audioBuffer.getChannelData(0)[i] = int16Array[i] / 32768; // Normalize the sample to the range of -1 to 1
   }
-  // Calculate the volume of the audio samples
+  // Calculate the volume
   const volume = calculateVolume(audioBuffer);
 
   // Detect silence
@@ -115,8 +159,8 @@ async function processMedia(msg) {
     } else {
       const currentTime = new Date().getTime();
       if (currentTime - silenceStart > silenceDuration) {
-        console.log("Detected silence");
-        silenceStart = currentTime;
+        console.log("Detected silence. Stopping listen action");
+        stopListening();
       }
     }
   } else {
@@ -134,4 +178,15 @@ function calculateVolume(audioBuffer) {
 
   const rms = Math.sqrt(sum / samples.length);
   return rms;
+}
+
+function generateUniqueId() {
+  return uuidv4();
+}
+
+function stopListening() {
+  listen = false;
+  silenceStart = null;
+  listenStart = null;
+  listenDuration = 1000;
 }
